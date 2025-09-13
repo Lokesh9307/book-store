c175de71478521b4e5093b992afebae460b89d89
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-09-14 00:41:19 +0530

    mongodb added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.error("MongoDB Error:", err.message);
+    console.error("❌ MongoDB Error:", err.message);
     process.exit(1);
   }
 };
